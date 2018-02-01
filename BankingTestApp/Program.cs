21907aa0f00ae8daa5b83dc9a346552241e92c43
di13kom <dkomgameplay@gmail.com>
2018-02-02 00:24:30 +0300

    FirstCommit.

--- BankingTestApp/Program.cs
+++ BankingTestApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,34 @@ namespace BankingTestApp
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string chars = "localhost:7777";
+                HttpListener listener = new HttpListener();
+                listener.Prefixes.Add("http://" + chars + "/");
+                listener.Start();
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest req = context.Request;
+                if (req.ContentType == "\"application/json\"" && req.HttpMethod == "POST")
+                {
+                    HttpListenerResponse resp = context.Response;
+                    resp.ContentType = "application/json";
+                    using (Stream str = resp.OutputStream)
+                    {
+                        string stringOut = "{\"GU\":25, \"GB\":35}";
+                        byte[] bytes = Encoding.UTF8.GetBytes(stringOut);
+                        str.Write(bytes, 0, bytes.Count());
+                        //Console.Read();
+                    }
+                    //str.Close();
+                }
+                listener.Close();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"error: {ex.Message}");
+            }
         }
     }
 }

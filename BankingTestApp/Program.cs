9ca7a97e3ce7e48c1e219778c23e7a1c4d8a3bd2
di13kom <dkomgameplay@gmail.com>
2018-02-03 17:52:02 +0300

    BankingTestApp:     - Added input stream for request.

--- BankingTestApp/Program.cs
+++ BankingTestApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,18 +21,35 @@ namespace BankingTestApp
                 HttpListener listener = new HttpListener();
                 listener.Prefixes.Add("http://" + chars + "/");
                 listener.Start();
-                while(listener.IsListening)
+                while (listener.IsListening)
                 {
                     HttpListenerContext context = listener.GetContext();
-                    Console.WriteLine($"incoming connection...");
                     HttpListenerRequest req = context.Request;
+                    Console.WriteLine($"incoming connection from: {req.RemoteEndPoint.Address.ToString()}");
                     if (req.ContentType.Contains("application/json") && req.HttpMethod == "POST")
                     {
+                        //string inData;
+                        StringBuilder inDataB = new StringBuilder();
+                        byte[] inputByte = new byte[10];
+
+                        using (Stream inStr = req.InputStream)
+                        //using (StreamReader reader = new StreamReader(inStr))
+                        {
+                            int readEl = 0;
+                            do
+                            {
+                                readEl = inStr.Read(inputByte, 0, 10);
+                                inDataB.Append(Encoding.UTF8.GetChars(inputByte), 0, readEl);
+                            }
+                            while (readEl > 0);
+                            //inData = reader.ReadToEnd();
+                        }
+                        JObject jObj = JObject.Parse(inDataB.ToString());
                         HttpListenerResponse resp = context.Response;
                         resp.ContentType = "application/json";
                         using (Stream str = resp.OutputStream)
                         {
-                            string stringOut = "{\"GU\":25, \"GB\":35}";
+                            string stringOut = $"{{\"Status\":\"Ok\", \"ErrorCode\":0, \"Sum\":{jObj["Amount"]}}}";
                             byte[] bytes = Encoding.UTF8.GetBytes(stringOut);
                             str.Write(bytes, 0, bytes.Count());
                             //Console.Read();
